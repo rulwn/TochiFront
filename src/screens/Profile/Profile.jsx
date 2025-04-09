b01babb6e1c7cab334d968526e0749974e72650a
Jorge <jfer.orantes@icloud.com>
2025-04-08 20:23:34 -0600

    ñui deja de hacer commits en ingles

--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -1,22 +1,74 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Profile.css'; // Archivo CSS para estilos (opcional)
+import './Profile.css';
+
+import { FiUser, FiHome, FiCreditCard, FiInfo, FiShoppingBag } from 'react-icons/fi';
+import { FiChevronRight } from 'react-icons/fi';
 
 function Profile() {
   const navigate = useNavigate();
 
-  const handleNavigate = () => {
-    navigate('/termsAndConditions');
+  const handleNavigation = (path) => {
+    navigate(path);
   };
 
   return (
     <div className="profile-container">
-      <button 
-        className="terms-button"
-        onClick={handleNavigate}
-      >
-        Ver Términos y Condiciones
-      </button>
+      <div className="profile-header">
+        <img 
+          src="https://lh3.googleusercontent.com/a-/ALV-UjUN_oQTSuBnJiaR98U0JzFFBMSr29mgrzQNNryHqDEqkP48xnQ=s80-p-k-rw-no" 
+          alt="Profile" 
+          className="profile-img"
+        />
+        <div className="profile-info">
+          <h1>Wilfredo Granados</h1>
+          <p className="email">ing_wilfredo@gmail.com</p>
+        </div>
+      </div>
+
+      <div className="profile-options">
+        <h2>Menu</h2>
+        
+        <div className="option-item" onClick={() => handleNavigation('/orders')}>
+          <div className="option-content">
+            <FiShoppingBag className="option-icon" />
+            <span>Orders</span>
+          </div>
+          <FiChevronRight className="arrow-icon" />
+        </div>
+        
+        <div className="option-item" onClick={() => handleNavigation('/my-details')}>
+          <div className="option-content">
+            <FiUser className="option-icon" />
+            <span>My Details</span>
+          </div>
+          <FiChevronRight className="arrow-icon" />
+        </div>
+        
+        <div className="option-item" onClick={() => handleNavigation('/delivery-address')}>
+          <div className="option-content">
+            <FiHome className="option-icon" />
+            <span>Delivery Address</span>
+          </div>
+          <FiChevronRight className="arrow-icon" />
+        </div>
+        
+        <div className="option-item" onClick={() => handleNavigation('/payment-methods')}>
+          <div className="option-content">
+            <FiCreditCard className="option-icon" />
+            <span>Payment Methods</span>
+          </div>
+          <FiChevronRight className="arrow-icon" />
+        </div>
+        
+        <div className="option-item" onClick={() => handleNavigation('/about')}>
+          <div className="option-content">
+            <FiInfo className="option-icon" />
+            <span>About</span>
+          </div>
+          <FiChevronRight className="arrow-icon" />
+        </div>
+      </div>
     </div>
   );
 }
